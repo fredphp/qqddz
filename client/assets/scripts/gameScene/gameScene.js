2eb0afd752b8a12ed5ec92e4e59d5a1ff00ddd44
Z User <z@container>
2026-04-21 14:33:36 +0000

    fix: 修复客户端启动黑屏问题
    - 重写 mygolbal.js，增加自动初始化和重试机制
    - 改进 loginScene.js，增加等待 myglobal 就绪的逻辑
    - 改进 gameScene.js，增加等待初始化的逻辑
    - 所有场景脚本增加更完善的错误处理
    - 确保依赖脚本加载完成后再初始化

--- client/assets/scripts/gameScene/gameScene.js
+++ client/assets/scripts/gameScene/gameScene.js
@@ -4,85 +4,117 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        di_label:cc.Label,
-        beishu_label:cc.Label,
-        roomid_label:cc.Label,
-        player_node_prefabs:cc.Prefab,
+        di_label: cc.Label,
+        beishu_label: cc.Label,
+        roomid_label: cc.Label,
+        player_node_prefabs: cc.Prefab,
         //绑定玩家座位,下面有3个子节点
-        players_seat_pos:cc.Node,
+        players_seat_pos: cc.Node,
 
     },
     //本局结束，做状态清除
-    gameEnd(){
+    gameEnd() {
 
     },
-    onLoad () {
-        var myglobal = window.myglobal
-        var RoomState = window.RoomState
-        var isopen_sound = window.isopen_sound || 1
+    onLoad() {
+        console.log("gameScene onLoad 开始");
         
-        if (!myglobal || !myglobal.playerData) {
-            console.error("myglobal 或 playerData 未定义")
+        var myglobal = window.myglobal
+        var RoomState = window.RoomState || { ROOM_INVALID: -1 }
+        var isopen_sound = window.isopen_sound || 1
+
+        if (!myglobal || !myglobal.playerData || !myglobal.socket) {
+            console.error("gameScene: myglobal、playerData 或 socket 未定义")
+            this._waitForInit()
             return
         }
         
+        this._initScene(myglobal, RoomState, isopen_sound)
+    },
+    
+    _waitForInit: function() {
+        var self = this;
+        var attempts = 0;
+        var maxAttempts = 20;
+        
+        var checkInit = function() {
+            attempts++;
+            console.log("gameScene 等待初始化... (第 " + attempts + " 次)");
+            
+            var myglobal = window.myglobal;
+            if (myglobal && myglobal.playerData && myglobal.socket) {
+                console.log("gameScene 初始化条件满足");
+                var RoomState = window.RoomState || { ROOM_INVALID: -1 };
+                var isopen_sound = window.isopen_sound || 1;
+                self._initScene(myglobal, RoomState, isopen_sound);
+            } else if (attempts < maxAttempts) {
+                setTimeout(checkInit, 100);
+            } else {
+                console.error("gameScene 初始化超时");
+            }
+        };
+        
+        setTimeout(checkInit, 100);
+    },
+    
+    _initScene: function(myglobal, RoomState, isopen_sound) {
         this.playerNodeList = []
-        this.di_label.string = "底:" +  myglobal.playerData.bottom
+        this.di_label.string = "底:" + myglobal.playerData.bottom
         this.beishu_label.string = "倍数:" + myglobal.playerData.rate
         this.roomstate = RoomState.ROOM_INVALID
-        
+
         //监听，给其他玩家发牌(内部事件)
-        this.node.on("pushcard_other_event",function(){
+        this.node.on("pushcard_other_event", function() {
             console.log("gamescene pushcard_other_event")
-            for(var i=0;i<this.playerNodeList.length;i++){
+            for (var i = 0; i < this.playerNodeList.length; i++) {
                 var node = this.playerNodeList[i]
-                if(node){
+                if (node) {
                     node.emit("push_card_event")
                 }
             }
         }.bind(this))
 
         //监听房间状态改变事件
-        myglobal.socket.onRoomChangeState(function(data){
-            console.log("onRoomChangeState:"+data)
+        myglobal.socket.onRoomChangeState(function(data) {
+            console.log("onRoomChangeState:" + data)
             this.roomstate = data
         }.bind(this))
 
-        this.node.on("canrob_event",function(event){
-            console.log("gamescene canrob_event:"+event)
-            for(var i=0;i<this.playerNodeList.length;i++){
+        this.node.on("canrob_event", function(event) {
+            console.log("gamescene canrob_event:" + event)
+            for (var i = 0; i < this.playerNodeList.length; i++) {
                 var node = this.playerNodeList[i]
-                if(node){
-                    node.emit("playernode_canrob_event",event)
+                if (node) {
+                    node.emit("playernode_canrob_event", event)
                 }
             }
         }.bind(this))
 
-        this.node.on("choose_card_event",function(event){
+        this.node.on("choose_card_event", function(event) {
             console.log("--------choose_card_event-----------")
             var gameui_node = this.node.getChildByName("gameingUI")
-            if(gameui_node==null){
-               console.log("get childer name gameingUI")
-               return
+            if (gameui_node == null) {
+                console.log("get childer name gameingUI")
+                return
             }
-            gameui_node.emit("choose_card_event",event)
+            gameui_node.emit("choose_card_event", event)
         }.bind(this))
 
-        this.node.on("unchoose_card_event",function(event){
+        this.node.on("unchoose_card_event", function(event) {
             console.log("--------unchoose_card_event-----------")
             var gameui_node = this.node.getChildByName("gameingUI")
-            if(gameui_node==null){
-               console.log("get childer name gameingUI")
-               return
+            if (gameui_node == null) {
+                console.log("get childer name gameingUI")
+                return
             }
-            gameui_node.emit("unchoose_card_event",event)
+            gameui_node.emit("unchoose_card_event", event)
         }.bind(this))
 
-        myglobal.socket.request_enter_room({},function(err,result){
-            console.log("enter_room_resp"+ JSON.stringify(result))
-            if(err!=0){
-               console.log("enter_room_resp err:"+err)
-            }else{
+        myglobal.socket.request_enter_room({}, function(err, result) {
+            console.log("enter_room_resp" + JSON.stringify(result))
+            if (err != 0) {
+                console.log("enter_room_resp err:" + err)
+            } else {
                 var seatid = result.seatindex
                 this.playerdata_list_pos = []
                 this.setPlayerSeatPos(seatid)
@@ -91,142 +123,150 @@ cc.Class({
                 var roomid = result.roomid
                 this.roomid_label.string = "房间号:" + roomid
                 myglobal.playerData.housemanageid = result.housemanageid
-                
-                for(var i=0;i<playerdata_list.length;i++){
+
+                for (var i = 0; i < playerdata_list.length; i++) {
                     this.addPlayerNode(playerdata_list[i])
                 }
 
-                if(isopen_sound){
-                    cc.audioEngine.stopAll()
-                    cc.audioEngine.play(cc.url.raw("resources/sound/bg.mp3"),true) 
+                if (isopen_sound) {
+                    try {
+                        cc.audioEngine.stopAll()
+                        cc.audioEngine.play(cc.url.raw("resources/sound/bg.mp3"), true)
+                    } catch(e) {
+                        console.log("播放背景音乐失败:", e)
+                    }
                 }
             }
             var gamebefore_node = this.node.getChildByName("gamebeforeUI")
-            gamebefore_node.emit("init")
+            if (gamebefore_node) {
+                gamebefore_node.emit("init")
+            }
         }.bind(this))
 
         //在进入房间后，注册其他玩家进入房间的事件
-        myglobal.socket.onPlayerJoinRoom(function(join_playerdata){
-            console.log("onPlayerJoinRoom:"+JSON.stringify(join_playerdata))
+        myglobal.socket.onPlayerJoinRoom(function(join_playerdata) {
+            console.log("onPlayerJoinRoom:" + JSON.stringify(join_playerdata))
             this.addPlayerNode(join_playerdata)
         }.bind(this))
-        
+
         //回调参数是发送准备消息的accountid
-        myglobal.socket.onPlayerReady(function(data){
-            console.log("-------onPlayerReady:"+data)
-            for(var i=0;i<this.playerNodeList.length;i++){
+        myglobal.socket.onPlayerReady(function(data) {
+            console.log("-------onPlayerReady:" + data)
+            for (var i = 0; i < this.playerNodeList.length; i++) {
                 var node = this.playerNodeList[i]
-                if(node){
-                    node.emit("player_ready_notify",data)
+                if (node) {
+                    node.emit("player_ready_notify", data)
                 }
             }
         }.bind(this))
 
-        myglobal.socket.onGameStart(function(){
-            for(var i=0;i<this.playerNodeList.length;i++){
+        myglobal.socket.onGameStart(function() {
+            for (var i = 0; i < this.playerNodeList.length; i++) {
                 var node = this.playerNodeList[i]
-                if(node){
+                if (node) {
                     node.emit("gamestart_event")
                 }
             }
 
             var gamebeforeUI = this.node.getChildByName("gamebeforeUI")
-            if(gamebeforeUI){
+            if (gamebeforeUI) {
                 gamebeforeUI.active = false
             }
         }.bind(this))
 
         //监听服务器玩家抢地主消息
-        myglobal.socket.onRobState(function(event){
-            console.log("-----onRobState"+JSON.stringify(event))
-            for(var i=0;i<this.playerNodeList.length;i++){
+        myglobal.socket.onRobState(function(event) {
+            console.log("-----onRobState" + JSON.stringify(event))
+            for (var i = 0; i < this.playerNodeList.length; i++) {
                 var node = this.playerNodeList[i]
-                if(node){
-                    node.emit("playernode_rob_state_event",event)
+                if (node) {
+                    node.emit("playernode_rob_state_event", event)
                 }
             }
         }.bind(this))
 
         //注册监听服务器确定地主消息
-        myglobal.socket.onChangeMaster(function(event){
-            console.log("onChangeMaster"+event)
+        myglobal.socket.onChangeMaster(function(event) {
+            console.log("onChangeMaster" + event)
             myglobal.playerData.master_accountid = event
-            for(var i=0;i<this.playerNodeList.length;i++){
+            for (var i = 0; i < this.playerNodeList.length; i++) {
                 var node = this.playerNodeList[i]
-                if(node){
-                    node.emit("playernode_changemaster_event",event)
+                if (node) {
+                    node.emit("playernode_changemaster_event", event)
                 }
             }
         }.bind(this))
-        
+
         //注册监听服务器显示底牌消息
-        myglobal.socket.onShowBottomCard(function(event){
-           console.log("onShowBottomCard---------"+event)
-           var gameui_node = this.node.getChildByName("gameingUI")
-           if(gameui_node==null){
-              console.log("get childer name gameingUI")
-              return
-           }
-           gameui_node.emit("show_bottom_card_event",event)
+        myglobal.socket.onShowBottomCard(function(event) {
+            console.log("onShowBottomCard---------" + event)
+            var gameui_node = this.node.getChildByName("gameingUI")
+            if (gameui_node == null) {
+                console.log("get childer name gameingUI")
+                return
+            }
+            gameui_node.emit("show_bottom_card_event", event)
         }.bind(this))
+        
+        console.log("gameScene 初始化完成");
     },
 
     //seat_index自己在房间的位置id
-    setPlayerSeatPos(seat_index){
-        if(seat_index < 1 || seat_index > 3){
-            console.log("seat_index error"+seat_index)
+    setPlayerSeatPos(seat_index) {
+        if (seat_index < 1 || seat_index > 3) {
+            console.log("seat_index error" + seat_index)
             return
         }
 
         console.log("setPlayerSeatPos seat_index:" + seat_index)
-       
-        switch(seat_index){
+
+        switch (seat_index) {
             case 1:
-                   this.playerdata_list_pos[1] = 0
-                   this.playerdata_list_pos[2] = 1 
-                   this.playerdata_list_pos[3] = 2
-              break
-             case 2:
-                    this.playerdata_list_pos[2] = 0
-                    this.playerdata_list_pos[3] = 1
-                    this.playerdata_list_pos[1] = 2
-                    break
-             case 3:
-                    this.playerdata_list_pos[3] = 0     
-                    this.playerdata_list_pos[1] = 1
-                    this.playerdata_list_pos[2] = 2
-                    break
-            default: 
-              break      
-        } 
+                this.playerdata_list_pos[1] = 0
+                this.playerdata_list_pos[2] = 1
+                this.playerdata_list_pos[3] = 2
+                break
+            case 2:
+                this.playerdata_list_pos[2] = 0
+                this.playerdata_list_pos[3] = 1
+                this.playerdata_list_pos[1] = 2
+                break
+            case 3:
+                this.playerdata_list_pos[3] = 0
+                this.playerdata_list_pos[1] = 1
+                this.playerdata_list_pos[2] = 2
+                break
+            default:
+                break
+        }
 
     },
 
-    addPlayerNode(player_data){
+    addPlayerNode(player_data) {
         var playernode_inst = cc.instantiate(this.player_node_prefabs)
         playernode_inst.parent = this.node
         this.playerNodeList.push(playernode_inst)
 
         var index = this.playerdata_list_pos[player_data.seatindex]
-        console.log("index "+player_data.seatindex+ " "+index)
+        console.log("index " + player_data.seatindex + " " + index)
         playernode_inst.position = this.players_seat_pos.children[index].position
-        playernode_inst.getComponent("player_node").init_data(player_data,index)
+        playernode_inst.getComponent("player_node").init_data(player_data, index)
     },
 
-    start () {
+    start() {
     },
 
-    getUserOutCardPosByAccount(accountid){
-        console.log("getUserOutCardPosByAccount accountid:"+accountid)
-        for(var i=0;i<this.playerNodeList.length;i++){
+    getUserOutCardPosByAccount(accountid) {
+        console.log("getUserOutCardPosByAccount accountid:" + accountid)
+        for (var i = 0; i < this.playerNodeList.length; i++) {
             var node = this.playerNodeList[i]
-            if(node){
+            if (node) {
                 var node_script = node.getComponent("player_node")
-                if(node_script.accountid===accountid){
-                  var seat_node = this.players_seat_pos.children[node_script.seat_index]
-                  var index_name = "cardsoutzone"+node_script.seat_index
-                  var out_card_node = seat_node.getChildByName(index_name)
-                  return out_card_node
+                if (node_script.accountid === accountid) {
+                    var seat_node = this.players_seat_pos.children[node_script.seat_index]
+                    var index_name = "cardsoutzone" + node_script.seat_index
+                    var out_card_node = seat_node.getChildByName(index_name)
+                    return out_card_node
                 }
             }
         }
--- client/assets/scripts/loginscene/loginScene.js
+++ client/assets/scripts/loginscene/loginScene.js
@@ -1,74 +1,125 @@
+// 登录场景控制器
 // 使用全局变量，不使用 require
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
-       wait_node:cc.Node,
+       wait_node: cc.Node,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        var myglobal = window.myglobal
-        var isopen_sound = window.isopen_sound || 1
+        console.log("loginScene onLoad 开始");
         
-        // 确保 myglobal 已初始化
-        if (!myglobal) {
-            console.error("myglobal 未定义")
-            return
+        // 确保 myglobal 存在
+        if (typeof window.myglobal === 'undefined') {
+            console.error("myglobal 未定义，尝试等待...");
+            this._waitForMyglobal();
+            return;
         }
+        
+        this._initAndStart();
+    },
+    
+    _waitForMyglobal: function() {
+        var self = this;
+        var attempts = 0;
+        var maxAttempts = 20;
+        
+        var checkMyglobal = function() {
+            attempts++;
+            console.log("等待 myglobal... (第 " + attempts + " 次)");
+            
+            if (typeof window.myglobal !== 'undefined') {
+                console.log("myglobal 已就绪");
+                self._initAndStart();
+            } else if (attempts < maxAttempts) {
+                setTimeout(checkMyglobal, 100);
+            } else {
+                console.error("myglobal 加载超时，请刷新页面重试");
+                // 显示错误提示（如果有）
+                self._showError("加载失败，请刷新页面重试");
+            }
+        };
+        
+        setTimeout(checkMyglobal, 100);
+    },
+    
+    _initAndStart: function() {
+        var myglobal = window.myglobal;
+        var isopen_sound = window.isopen_sound || 1;
         
         // 如果 socket 未初始化，尝试初始化
         if (!myglobal.socket) {
             if (!myglobal.init()) {
-                console.error("myglobal 初始化失败")
-                return
+                console.error("myglobal 初始化失败");
+                this._showError("初始化失败，请刷新页面重试");
+                return;
             }
         }
         
-        if(isopen_sound){
-            cc.audioEngine.play(cc.url.raw("resources/sound/login_bg.ogg"),true) 
+        console.log("loginScene 初始化完成");
+        console.log("  - myglobal.socket:", !!myglobal.socket);
+        console.log("  - myglobal.playerData:", !!myglobal.playerData);
+        
+        // 播放背景音乐
+        if (isopen_sound) {
+            try {
+                cc.audioEngine.play(cc.url.raw("resources/sound/login_bg.ogg"), true);
+            } catch(e) {
+                console.log("播放背景音乐失败:", e);
+            }
         }
-           
-        myglobal.socket.initSocket()
+        
+        // 初始化 WebSocket 连接
+        if (myglobal.socket && myglobal.socket.initSocket) {
+            myglobal.socket.initSocket();
+        }
+    },
+    
+    _showError: function(message) {
+        // 尝试显示错误信息
+        console.error("错误:", message);
+        // 可以在这里添加一个错误提示 UI
     },
     
     start () {
+        console.log("loginScene start");
     },
     
-    onButtonCilck(event,customData){
-        var myglobal = window.myglobal
-        switch(customData){
+    onButtonCilck(event, customData) {
+        console.log("onButtonCilck:", customData);
+        
+        var myglobal = window.myglobal;
+        if (!myglobal || !myglobal.socket) {
+            console.error("myglobal 或 socket 未初始化");
+            return;
+        }
+        
+        switch(customData) {
             case "wx_login":
-                console.log("wx_login request")
-                
-                //this.wait_node.active = true
+                console.log("wx_login request");
                 
                 myglobal.socket.request_wxLogin({
-                    uniqueID:myglobal.playerData.uniqueID,
-                    accountID:myglobal.playerData.accountID,
-                    nickName:myglobal.playerData.nickName,
-                    avatarUrl:myglobal.playerData.avatarUrl,
-                },function(err,result){
-                    //请求返回
-                    //先隐藏等待UI
-                    //this.wait_node.active = false
-                    if(err!=0){
-                       console.log("err:"+err)
-                       return     
+                    uniqueID: myglobal.playerData.uniqueID,
+                    accountID: myglobal.playerData.accountID,
+                    nickName: myglobal.playerData.nickName,
+                    avatarUrl: myglobal.playerData.avatarUrl,
+                }, function(err, result) {
+                    if (err != 0) {
+                        console.log("登录错误:" + err);
+                        return;
                     }
 
-                    console.log("login sucess" + JSON.stringify(result))
-                    myglobal.playerData.gobal_count = result.goldcount
-                    cc.director.loadScene("hallScene")
-                }.bind(this))
-                break
+                    console.log("登录成功" + JSON.stringify(result));
+                    myglobal.playerData.gobal_count = result.goldcount || 0;
+                    cc.director.loadScene("hallScene");
+                }.bind(this));
+                break;
             default:
-                break
+                break;
         }
     }
-    // update (dt) {},
-
-
 });
