1586432d5406caa2338528d44e639125f689c36b
Z User <z@container>
2026-04-25 02:22:50 +0000

    fix: 修复后台管理系统DDZ模块数据库连接问题，确保使用正确的数据库

--- admin/server/service/ddz/db.go
+++ admin/server/service/ddz/db.go
@@ -1,16 +1,18 @@
 package ddz
 
 import (
-	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	"gorm.io/gorm"
+        "github.com/flipped-aurora/gin-vue-admin/server/global"
+        "gorm.io/gorm"
 )
 
 // GetDDZDB 获取DDZ数据库连接
-// 优先使用配置的 ddz-game 数据库，如果未配置则使用默认数据库
+// 使用主数据库（hlddz），因为游戏服务器也将数据写入该数据库
 func GetDDZDB() *gorm.DB {
-	db := global.GetGlobalDBByDBName("ddz-game")
-	if db == nil {
-		db = global.GVA_DB
-	}
-	return db
+        // 优先尝试使用 ddz-game 数据库配置
+        db := global.GetGlobalDBByDBName("ddz-game")
+        if db != nil {
+                return db
+        }
+        // 使用主数据库（游戏服务器配置的数据库名称）
+        return global.GVA_DB
 }
