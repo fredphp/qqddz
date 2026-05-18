df9a5817804676fc5b1725e37fa34bacd00d8f56
Z User <z@container>
2026-05-18 01:36:13 +0000

    fix: restore missing upload components from history

---
+++ admin/web/src/components/upload/common.vue
@@ -0,0 +1,56 @@
+<template>
+  <el-upload
+    :action="`${baseUrl}/fileUploadAndDownload/upload?noSave=1`"
+    :headers="headers"
+    :show-file-list="false"
+    :on-success="handleSuccess"
+    :on-error="handleError"
+  >
+    <el-button type="primary" :icon="Upload">上传文件</el-button>
+  </el-upload>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { ElMessage } from 'element-plus'
+import { Upload } from '@element-plus/icons-vue'
+import { useUserStore } from '@/pinia'
+import { getBaseUrl } from '@/utils/format'
+
+defineOptions({
+  name: 'UploadCommon'
+})
+
+defineProps({
+  imageCommon: {
+    type: String,
+    default: ''
+  },
+  classId: {
+    type: Number,
+    default: 0
+  }
+})
+
+const emits = defineEmits(['on-success'])
+
+const userStore = useUserStore()
+const baseUrl = getBaseUrl()
+
+const headers = computed(() => ({
+  'x-token': userStore.token
+}))
+
+const handleSuccess = (response) => {
+  if (response.code === 0) {
+    ElMessage.success('上传成功')
+    emits('on-success', response)
+  } else {
+    ElMessage.error(response.msg || '上传失败')
+  }
+}
+
+const handleError = () => {
+  ElMessage.error('上传失败')
+}
+</script>
